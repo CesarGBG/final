44418a3712b9a86da310781474b0ccb16625c32a
CesarGBG <gianfrancobueno212@gmail.com>
2025-07-02 14:48:05 -0500

    analisis_comentarios.py

--- analisis_comentarios.py
+++ analisis_comentarios.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 # 1. Cargar el archivo CSV con codificación adecuada
-df = pd.read_csv("comentarios_video1.csv", encoding='latin-1')
+df = pd.read_csv("comentarios_video1.csv")
 
 # 2. Ver primeras filas
 print("Primeras filas del archivo:")
