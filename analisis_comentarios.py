cc6c1fb52ec2135621dfb8c9ff62314e75abb3bc
CesarGBG <gianfrancobueno212@gmail.com>
2025-07-02 14:46:31 -0500

    Add files via upload

---
+++ analisis_comentarios.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+
+# 1. Cargar el archivo CSV con codificación adecuada
+df = pd.read_csv("comentarios_video1.csv", encoding='latin-1')
+
+# 2. Ver primeras filas
+print("Primeras filas del archivo:")
+print(df.head())
+
+# 3. Ver nombres de columnas
+print("\nColumnas disponibles:")
+print(df.columns)
